b03cc86f48c16a0fb8ec5ba644c06bea861918c6
raywoo32 <40863729+raywoo32@users.noreply.github.com>
2019-06-24 15:57:09 -0400

    non-user specific

--- dbConfig.js
+++ dbConfig.js
@@ -4,8 +4,8 @@ const options = {
     client: 'mysql2',
     connection: {
         host: '127.0.0.1',
-        user: 'rwoo',
-        password: '123Password',
+        user: '',
+        password: '',
         database: 'interactions_vincent'
     }
 }
